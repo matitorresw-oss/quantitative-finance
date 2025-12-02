612922e97a16a2102ad8a7f52028a186d1acca71
matitorresw-oss <matitorresw@gmail.com>
2025-12-02 16:57:47 +0000

    rama actualizada

--- main.py
+++ main.py
@@ -23,17 +23,38 @@ if __name__ == '__main__':
 
     # nivel de confianza
     confidence = 0.05
+    lst = []
 
     for portafolio in [list_tickers_rf, list_tickers_rv]:
-        print(portafolio)
-      
-      # obtener retornos
-      df = portfolio_returns(tickers=portafolio, start=start, end=end)
-      print(df.head(5))
+        
 
-      vector_w = np.array([1/len(portafolio)]*len(portafolio))
-      print(vector_w)
+        # obtener retornos
+        df = portfolio_returns(tickers=portafolio, start=start, end=end)
+        
 
-      # calcular volatilidad
-      sigma = portfolio_volatility(df=df, vector_w=vector_w)
-      print(sigma)
+        vector_w = np.array([1/len(portafolio)]*len(portafolio))
+        
+
+        # calcular volatilidad
+        sigma = portfolio_volatility(df=df, vector_w=vector_w)
+       
+
+        # calcular VaR
+        var= VaR(sigma=sigma, confidence=confidence)
+        var=np.abs(var)
+        var_mensual = var * np.sqrt(20)
+        lst.append(var_mensual)
+    
+    
+    df_final = pd.DataFrame(
+        {
+        'PORTAFOLIO': ['Ishares Renta Fija', 'Ishares Renta Variable'], 
+        f'Value At Risk: {1-confidence}%': lst
+
+        }
+    )
+    df_final = df_final.sort_values(
+        by=f'Value At Risk: {1-confidence}%', 
+        ascending = False)
+    print(df_final)    
+
